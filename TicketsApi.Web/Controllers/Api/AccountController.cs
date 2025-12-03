69104c54925747019aa038bfbf2b0a00f0127797
Luis Núñez <luisalbertonu@gmail.com>
2025-12-03 12:23:33 -0300

    Cambios

--- TicketsApi.Web/Controllers/Api/AccountController.cs
+++ TicketsApi.Web/Controllers/Api/AccountController.cs
@@ -323,6 +323,17 @@ namespace TicketsApi.Àpi.Controllers.Àpi
             List<UserViewModel> list = new List<UserViewModel>();
             foreach (User user in users)
             {
+                string bossId = null;
+                string bossName = null;
+
+                if (user.BossId != null)
+                {
+                    User boss = await _userHelper.GetUserByIdAsync(user.BossId);
+                    bossId = boss.Id;
+                    bossName = boss.FullName;
+                }
+
+
                 UserViewModel userViewModel = new UserViewModel
                 {
                     Id = user.Id,
@@ -348,8 +359,8 @@ namespace TicketsApi.Àpi.Controllers.Àpi
                     Active = user.Active,
                     IsResolver = user.IsResolver,
                     IsBoss = user.IsBoss,
-                    BossAsign = user.BossId,
-                    BossAsignName = user.BossName,
+                    BossAsign = bossId,
+                    BossAsignName = bossName,
                     Tickets = user.Tickets?.Select(ticket => new TicketCabViewModel
                     {
                         Id = ticket.Id,
