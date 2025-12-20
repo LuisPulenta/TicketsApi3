edbc2c97ce9ceb2ad081c46bd666d487370554c4
Luis Núñez <luisalbertonu@gmail.com>
2025-12-20 12:08:01 -0300

    Cambio

--- TicketsApi.Web/Controllers/Api/TicketCabsController.cs
+++ TicketsApi.Web/Controllers/Api/TicketCabsController.cs
@@ -899,7 +899,7 @@ namespace TicketsApi.Web.Controllers.Api
             {
                 ticketCabs = await _context.TicketCabs
                 .Include(x => x.TicketDets)
-                .Where(x => x.CompanyId == request.CompanyId && x.TicketState == TicketState.Resuelto && x.CreateDate >= request.Desde && x.CreateDate <= request.Hasta.AddDays(1))
+                .Where(x => x.CompanyId == request.CompanyId && (x.TicketState == TicketState.Resuelto || x.TicketState == TicketState.Cerrado) && x.CreateDate >= request.Desde && x.CreateDate <= request.Hasta.AddDays(1))
                 .OrderBy(x => x.CompanyName)
                 .OrderBy(x => x.Id)
                 .ToListAsync();
@@ -908,7 +908,7 @@ namespace TicketsApi.Web.Controllers.Api
             {
                 ticketCabs = await _context.TicketCabs
                 .Include(x => x.TicketDets)
-                .Where(x => x.UserId == request.UserId && x.TicketState == TicketState.Resuelto && x.CreateDate >= request.Desde && x.CreateDate <= request.Hasta.AddDays(1))
+                .Where(x => x.UserId == request.UserId && (x.TicketState == TicketState.Resuelto || x.TicketState == TicketState.Cerrado) && x.CreateDate >= request.Desde && x.CreateDate <= request.Hasta.AddDays(1))
                 .OrderBy(x => x.CompanyName)
                 .OrderBy(x => x.Id)
                 .ToListAsync();
